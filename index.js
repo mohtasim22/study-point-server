c4b778d8e9b442151e9f2dc072d564aeb4df6e55
mohtasim22 <fahimatbd@gmail.com>
2023-10-21 14:24:57 +0600

    integrated mongoDB connection

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xe6z2zy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
